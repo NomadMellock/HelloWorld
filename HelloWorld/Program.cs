31dd52687ffff993b90b8804b8da8cc8fbe0536f
Gary Lehan <gary.lehan@oneadvanced.com>
2019-04-01 11:01:14 +0100

    Initial Project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello World";
+        }
+
+    }
+}
